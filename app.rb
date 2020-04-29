fc97b9460857c874d30e812afad544ab2d49e44e
Mariam Haider <mfh.haider@gmail.com>
2020-04-29 18:39:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,18 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+class App < Sinatra::Base
 
+   get '/newteam' do
+
+      erb :newteam
+   end
+
+   post '/team' do
+      @params = params
+      erb :team
+   end
+
+end
 
 end
